6bdb61446e2652848f29d7d252870a3f95188baf
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-04-17 00:30:13 +0800

    Updates page implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import componentMap from '@app/lib/constants/componentMap';
 import homepageQuery from '@app/lib/queries/homepage.query';
 import Hero from '@app/components/Hero/Hero';
+import PersonalDetails from '@app/components/PersonalDetails/PersonalDetails';
 import { notFound } from 'next/navigation';
 import { fetchGraphQL } from '@app/lib/helpers/api';
-
-interface FlexibleContentComponentProps {
-  __typename: string;
-  props: Record<string, any>;
-}
 
 export default async function Home() {
   const response = await fetchGraphQL(homepageQuery);
@@ -16,18 +11,13 @@ export default async function Home() {
   if (errors.length > 0) notFound();
 
   const { homepage = {} } = data || {};
-  const { background, cv, description, headline, flexibleComponentsAreaCollection } = homepage;
+  const { background, cv, description, headline, personalDetails } = homepage;
   const heroData = { background, cv, description, headline };
 
   return (
     <main>
       <Hero {...heroData} />
-      {flexibleComponentsAreaCollection.items.map(
-        (item: FlexibleContentComponentProps, index: number) => {
-          const Component = componentMap[item.__typename];
-          return <Component key={index} {...item} />;
-        }
-      )}
+      <PersonalDetails {...personalDetails} />
     </main>
   );
 }
