200f986d32eb3ac2885c5033fa5fdc5caff58cb3
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-05-09 18:28:58 +0800

    Fixes TS errors

--- src/components/Essay/Essay.tsx
+++ src/components/Essay/Essay.tsx
@@ -2,7 +2,7 @@ import styles from './Essay.styles';
 import { EssayProps } from './Essay.interface';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-export default function Essay({ className, essay }: EssayProps) {
+export default function Essay({ className = '', essay }: EssayProps) {
   return (
     <div className={styles.essay(className)}>
       {documentToReactComponents(essay.json)}
--- src/lib/constants/componentMap.ts
+++ src/lib/constants/componentMap.ts
@@ -1,11 +1,7 @@
 import PersonalDetails from '@app/components/PersonalDetails/PersonalDetails';
 
-interface ComponentMapKey {
-  [key: string]: (_props: any) => JSX.Element;
-}
-
 // Define the component map object
-const componentMap: ComponentMapKey = {
+const componentMap = {
   PersonalDetails
 };
 
