d21f93a034cb8992d7bb02e3e42d45a9ae5c83c2
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-08-29 22:05:08 +0800

    Creates useClickOutside hook

---
+++ src/lib/hooks/useClickOutside.ts
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from 'react';
+
+const useClickOutside = (
+  callback: () => void,
+  targetRefs: React.RefObject<HTMLElement | null>[]
+) => {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        targetRefs.every((ref) => !ref.current?.contains(event.target as Node))
+      ) {
+        callbackRef.current();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [targetRefs]);
+};
+
+export default useClickOutside;
