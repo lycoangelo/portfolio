b77547acdfdef6f9ab9c0de44ea73ef51a4f8b54
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-08-31 18:41:48 +0800

    Deletes number.ts helper functions

--- src/lib/helpers/number.ts
+++
@@ -1,6 +0,0 @@
-// function that returns random number from min and max values
-export const getRandomInt = (min: number, max: number) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
