05fe0fbc29baaa7aa8054c1bbed45fe5e8351314
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-09-01 19:27:46 +0800

    Updates not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,26 @@
+import Button from '@app/components/atoms/Button/Button';
+import Header from '@app/components/organisms/Header/Header';
+import ThemePicker from '@app/components/organisms/ThemePicker/ThemePicker';
+
 export default function NotFound() {
-  return <h1 className="relative top-[200px]">Page Not Found!</h1>;
+  return (
+    <>
+      <Header />
+      <div className="grid-container pt-[200px]">
+        <h1 className="text-6xl lg:col-start-2 col-span-full mb-5 two-color">
+          Page <strong>Not Found</strong>!
+        </h1>
+        <Button
+          className="lg:col-start-2 col-span-full w-fit"
+          color="primary"
+          hasBorderEffect
+          href="/"
+          size="lg"
+        >
+          Go to Home Page
+        </Button>
+        <ThemePicker />
+      </div>
+    </>
+  );
 }
