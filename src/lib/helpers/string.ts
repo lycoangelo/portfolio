f714412f389e7ea87996fae677d70fed4a533a35
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-09-01 18:33:12 +0800

    Adds stringToKebabCase function helper

--- src/lib/helpers/string.ts
+++ src/lib/helpers/string.ts
@@ -1,5 +1,12 @@
 import parse from 'html-react-parser';
 
+export const stringToKebabCase = (text: string) => {
+  return text
+    .replace(/\s+/g, '-') // Replace spaces with hyphens
+    .replace(/([a-z])([A-Z])/g, '$1-$2') // Convert camelCase to kebab-case
+    .toLowerCase(); // Convert the entire string to lowercase
+};
+
 export const removeHtmlTags = (input: string) =>
   input
     .replace(/<\/?[^>]+(>|$)/g, ' ')
