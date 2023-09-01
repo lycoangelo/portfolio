5d0c02df128da4e8a58fe150e68daade5de76f6c
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-09-01 19:30:37 +0800

    Updates Header implementation

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { HeaderProps } from './Header.interface';
 import { useBreakpoint } from '@app/lib/hooks/useBreakpoint';
 import { useHideOtherElements } from '@app/lib/hooks/useHideOtherElements';
 import { useIsMounted } from '@app/lib/hooks/useIsMounted';
@@ -23,7 +24,7 @@ const links = [
   { label: 'Contact', href: CONTACT_FORM }
 ];
 
-export default function Header() {
+export default function Header({ isHomepage }: HeaderProps) {
   const [isActive, setIsActive] = useState(false);
 
   const closeRef = useRef(null);
@@ -64,38 +65,40 @@ export default function Header() {
         <Link className={classes.branding} href="/" title="Go to Homepage">
           <b>L</b>A
         </Link>
-        <FocusTrap active={isActive}>
-          <div className={classes.wrapper} ref={wrapperRef}>
-            <button
-              aria-expanded={isActive}
-              aria-label={`${isActive ? 'Close' : 'Open'} main menu`}
-              className={classes.toggle}
-              data-toggle
-              onClick={() => setIsActive(!isActive)}
-              ref={closeRef}
-            >
-              <span className={classes.hamburgerTop} />
-              <span className={classes.hamburgerCenter} />
-              <span className={classes.hamburgerBottom} />
-            </button>
-            <nav aria-hidden={!isActive && isBelowSm} className={classes.nav}>
-              <ul className={classes.list}>
-                {links.map(({ href, label }, index) => (
-                  <li className={classes.item} key={index}>
-                    <a
-                      className={classes.link}
-                      data-target={href}
-                      onClick={() => scrollToSection(href)}
-                      tabIndex={isActive ? 0 : -1}
-                    >
-                      {label}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </div>
-        </FocusTrap>
+        {isHomepage && (
+          <FocusTrap active={isActive}>
+            <div className={classes.wrapper} ref={wrapperRef}>
+              <button
+                aria-expanded={isActive}
+                aria-label={`${isActive ? 'Close' : 'Open'} main menu`}
+                className={classes.toggle}
+                data-toggle
+                onClick={() => setIsActive(!isActive)}
+                ref={closeRef}
+              >
+                <span className={classes.hamburgerTop} />
+                <span className={classes.hamburgerCenter} />
+                <span className={classes.hamburgerBottom} />
+              </button>
+              <nav aria-hidden={!isActive && isBelowSm} className={classes.nav}>
+                <ul className={classes.list}>
+                  {links.map(({ href, label }, index) => (
+                    <li className={classes.item} key={index}>
+                      <a
+                        className={classes.link}
+                        data-target={href}
+                        onClick={() => scrollToSection(href)}
+                        tabIndex={isActive ? 0 : -1}
+                      >
+                        {label}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+          </FocusTrap>
+        )}
       </div>
     </header>
   ) : null;
