47f080977baef712f41b42d60a31850a10d0e417
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-09-01 19:29:23 +0800

    Updates page.tsx implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable no-console */
 import homepageQuery from '@app/lib/queries/Homepage.query';
 import ContactForm from '@app/components/organisms/ContactForm/ContactForm';
 import Hero from '@app/components/organisms/Hero/Hero';
 import PersonalDetails from '@app/components/organisms/PersonalDetails/PersonalDetails.server';
-import { notFound } from 'next/navigation';
 import { fetchGraphQL } from '@app/lib/helpers/api';
 import Projects from '@app/components/organisms/Projects/Projects.server';
 import DropMotion from '@app/components/organisms/DropMotion/DropMotion';
 import ThemePicker from '@app/components/organisms/ThemePicker/ThemePicker';
+import { logColors } from '@app/lib/helpers/log';
+import Header from '@app/components/organisms/Header/Header';
 
 export const metadata = {
   title: 'Lyco Angelo: Portfolio'
@@ -16,7 +18,11 @@ export default async function Home() {
   const response = await fetchGraphQL(homepageQuery);
   const { data, errors = [] } = await response.json();
 
-  if (errors.length > 0) notFound();
+  if (errors.length > 0) {
+    console.log(
+      logColors.error + 'ERROR: ' + errors[0].message + logColors.reset
+    );
+  }
 
   const { homepage = {} } = data || {};
 
@@ -33,15 +39,18 @@ export default async function Home() {
   const heroData = { background, cv, description, headline };
 
   return (
-    <main className="overflow-hidden">
-      <Hero {...heroData} />
-      {/* @ts-expect-error Server Component */}
-      <PersonalDetails id={personalDetails.sys.id} />
-      {/* @ts-expect-error Server Component */}
-      <Projects {...projects} />
-      <ContactForm {...contactForm} />
-      <ThemePicker />
-      <DropMotion />
-    </main>
+    <>
+      <Header isHomepage />
+      <main className="overflow-hidden">
+        <Hero {...heroData} />
+        {/* @ts-expect-error Server Component */}
+        <PersonalDetails id={personalDetails.sys.id} />
+        {/* @ts-expect-error Server Component */}
+        <Projects {...projects} />
+        <ContactForm {...contactForm} />
+        <ThemePicker />
+        <DropMotion />
+      </main>
+    </>
   );
 }
