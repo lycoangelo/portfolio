e97f952306700153a294f0d62e818b5663b9515a
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-09-10 21:06:34 +0800

    Updates head.tsx

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,6 +3,30 @@ export default function Head() {
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <title>Lyco Angelo: Portfolio</title>
+      <meta
+        name="description"
+        content="The website portfolio of Lyco Angelo Ty, a website developer with experience in a variety of web design and development technologies. Learn more about his skills and experience today!"
+      />
+
+      <meta property="og:url" content="https://lycoangelo.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Lyco Angelo: Portfolio" />
+      <meta
+        property="og:description"
+        content="The website portfolio of Lyco Angelo Ty, a website developer with experience in a variety of web design and development technologies. Learn more about his skills and experience today!"
+      />
+      <meta property="og:image" content="/cover.jpg" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="lycoangelo.vercel.app/" />
+      <meta property="twitter:url" content="https://lycoangelo.vercel.app/" />
+      <meta name="twitter:title" content="Lyco Angelo: Portfolio" />
+      <meta
+        name="twitter:description"
+        content="The website portfolio of Lyco Angelo Ty, a website developer with experience in a variety of web design and development technologies. Learn more about his skills and experience today!"
+      />
+      <meta name="twitter:image" content="/cover.jpg" />
     </>
   );
 }
