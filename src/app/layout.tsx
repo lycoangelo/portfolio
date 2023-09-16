dd52421f8732cf4b1d05482923c095e8ecefe796
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2023-09-16 22:03:08 +0800

    Updates Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { Quicksand, Roboto } from 'next/font/google';
 import localFont from 'next/font/local';
 
-import GoogleAnalytics from '@app/components/atoms/GoogleAnalytics/GoogleAnalytics';
+import GoogleAnalytics, {
+  NoScriptGA
+} from '@app/components/atoms/GoogleAnalytics/GoogleAnalytics';
 import Footer from '@app/components/organisms/Footer/Footer';
 import ctl from '@netlify/classnames-template-literals';
 import { Analytics } from '@vercel/analytics/react';
@@ -43,6 +45,7 @@ export default function RootLayout({
       <head />
       <GoogleAnalytics />
       <body className={fontsVariables}>
+        <NoScriptGA />
         <Analytics />
         {children}
         {/* @ts-expect-error Server Component */}
--- src/components/atoms/GoogleAnalytics/GoogleAnalytics.tsx
+++ src/components/atoms/GoogleAnalytics/GoogleAnalytics.tsx
@@ -1,5 +1,18 @@
 import Script from 'next/script';
 
+export function NoScriptGA() {
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GA_GTM}`}
+        height="0"
+        width="0"
+        className="hidden"
+      />
+    </noscript>
+  );
+}
+
 export default function GoogleAnalytics() {
   return (
     <>
@@ -11,12 +24,12 @@ export default function GoogleAnalytics() {
       />
       <Script id="google-analytics">
         {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_GTM}');
-            `}
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GA_GTM}');
+        `}
       </Script>
     </>
   );
