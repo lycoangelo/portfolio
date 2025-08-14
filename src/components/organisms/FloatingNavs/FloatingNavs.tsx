2433d500efcd21abedbdc83260862d273baf5f7a
Lyco Angelo Ty <lyco.angelo.ty@codeandtheory.com>
2025-08-14 08:44:20 +0800

    Floating Navs > Implement react-cookie context provider

--- src/components/organisms/FloatingNavs/FloatingNavs.tsx
+++ src/components/organisms/FloatingNavs/FloatingNavs.tsx
@@ -2,7 +2,7 @@
 
 import { useIsMounted } from '@app/lib/hooks/useIsMounted';
 import { motion } from 'framer-motion';
-
+import { CookiesProvider } from 'react-cookie';
 import Share from '../Share/Share';
 import ThemePicker from '../ThemePicker/ThemePicker';
 import styles from './FloatingNavs.styles';
@@ -11,14 +11,16 @@ export default function FloatingNavs() {
   const isMounted = useIsMounted();
 
   return isMounted ? (
-    <motion.aside
-      className={styles.aside}
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.75 }}
-    >
-      <Share className={styles.share} isVertical />
-      <ThemePicker />
-    </motion.aside>
+    <CookiesProvider>
+      <motion.aside
+        className={styles.aside}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.75 }}
+      >
+        <Share className={styles.share} isVertical />
+        <ThemePicker />
+      </motion.aside>
+    </CookiesProvider>
   ) : null;
 }
